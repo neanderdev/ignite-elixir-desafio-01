ffb91cbc8cca5e72f125617358a0c51ff513b3d5
neanderdev <neander.faria@hotmail.com>
2022-08-26 10:38:43 -0300

    Desafio finalizado

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: length(list, 0)
+
+  defp length([], acc), do: acc
+
+  defp length([_head | tail], acc) do
+    acc = acc + 1
+    length(tail, acc)
   end
 end
